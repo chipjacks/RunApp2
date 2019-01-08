f7c06fcf0ca57475c57502dfd44e31baedb9a2d9
Chip Jackson <chipjacks@gmail.com>
2019-01-08 06:08:36 -0800

    Boilerplate

---
+++ src/Main.elm
@@ -0,0 +1,138 @@
+module Main exposing (main)
+
+import Browser
+import Browser.Navigation as Nav
+import Html
+import Url
+import Url.Parser as Parser exposing (Parser, oneOf, top)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = LinkClicked
+        , onUrlChange = UrlChanged
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { key : Nav.Key
+    , page : Page
+    }
+
+
+type Page
+    = NotFound
+    | Calendar
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    case model.page of
+        NotFound ->
+            { title = "Not Found"
+            , body = [ Html.div [] [ Html.text "Page Not Found" ] ]
+            }
+
+        Calendar ->
+            { title = "Calendar"
+            , body = [ Html.div [] [ Html.text "Calendar" ] ]
+            }
+
+
+
+-- INIT
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    stepUrl url
+        { key = key
+        , page = NotFound
+        }
+
+
+
+-- ROUTER
+
+
+stepUrl : Url.Url -> Model -> ( Model, Cmd Msg )
+stepUrl url model =
+    let
+        parser =
+            oneOf
+                [ route top
+                    ( { model | page = Calendar }
+                    , Cmd.none
+                    )
+                ]
+    in
+    case Parser.parse parser url of
+        Just result ->
+            result
+
+        Nothing ->
+            ( { model | page = NotFound }
+            , Cmd.none
+            )
+
+
+route : Parser a b -> a -> Parser (b -> c) c
+route parser handler =
+    Parser.map handler parser
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+    | LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NoOp ->
+            ( model, Cmd.none )
+
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model
+                    , Nav.pushUrl model.key (Url.toString url)
+                    )
+
+                Browser.External href ->
+                    ( model
+                    , Nav.load href
+                    )
+
+        UrlChanged url ->
+            stepUrl url model
