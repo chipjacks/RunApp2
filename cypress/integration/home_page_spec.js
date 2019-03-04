86cc1c318fb84efd001818ff3fd6a170a8aaf9ba
Chip Jackson <chipjacks@gmail.com>
2019-03-04 09:14:24 -0800

    Add tests for creating and editing activities

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,23 +1,27 @@
 context('The Home Page', function() {
   beforeEach(() => {
-    cy.visit('/')
+    cy.server()
+    cy.route('GET', '**/b/**', 'fixture:activities.json')
+    cy.route('PUT', '**/b/**', 'fixture:activities_updated.json')
   })
 
-  describe('calendar', function() {
-    it('links to the activitylist', function() {
+  describe('#calendar', function() {
+    beforeEach(() => {
       cy.visit('/calendar?date=737118')
+    })
+
+    it('links to the activities', function() {
       cy.get('#calendar').contains('a', '10').click()
       cy.url().should('match', /\/activities\?date=737100$/)
-      cy.get('#activities').contains('div', 'Activities 2019-02-10')
+      cy.get('#activities').contains('Activities 2019-02-10')
     })
 
-    it('loads automatically with activitylist', function() {
-      cy.visit('/activities?date=737118')
-      cy.get('#calendar').contains('a', '1')
+    it('loads automatically with activities', function() {
+      cy.get('#calendar').contains('a', '10')
     })
   })
 
-  describe('activitylist', function() {
+  describe('#activities', function() {
     beforeEach(() => {
       cy.visit('/activities?date=737118')
     })
@@ -28,12 +32,38 @@ context('The Home Page', function() {
     })
 
     it('loads automatically with calendar', function() {
-      cy.visit('/activities?date=737118')
-      cy.get('#activities').contains('div', 'Activities 2019-02-28')
+      cy.get('#activities').contains('Activities 2019-02-28')
+    })
+
+    it('lists activities', function() {
+      cy.get('#activities').contains('Tempo Tuesday')
+    })
+  })
+
+  describe('#activity', function() {
+    beforeEach(() => {
+      cy.visit('/')
+    })
+
+    it('creates new activities', function () {
+      cy.get('#activity').get('input').type('Long Run Sunday')
+      cy.get('#activity').contains('Save').click()
+      cy.get('#activities').contains('Long Run Sunday')
+    })
+
+    it('edits existing activities', function () {
+      cy.get('#activities').contains('Tempo Tuesday').click()
+      cy.get('#activity').get('input').should('have.value', 'Tempo Tuesday').type(' - Felt Great!')
+      cy.get('#activity').contains('Save').click()
+      cy.get('#activities').contains('Tempo Tuesday - Felt Great!')
     })
   })
 
   describe('resizing', function() {
+    beforeEach(() => {
+      cy.visit('/')
+    })
+
     it('adds columns as the window grows', function() {
       cy.viewport(320, 568) // iPhone 5
       cy.get('#calendar').should('exist')
