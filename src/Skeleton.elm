711c2242ab46831ee1961957437558510610243d
Chip Jackson <chipjacks@gmail.com>
2019-05-04 15:51:32 -0700

    Style navbar

--- src/Skeleton.elm
+++ src/Skeleton.elm
@@ -1,38 +1,55 @@
 module Skeleton exposing (column, expandingRow, layout, row, twoColumns)
 
-import Html exposing (Html, div, text)
-import Html.Attributes exposing (class, style)
+import Html exposing (Html, div, img, text)
+import Html.Attributes exposing (class, src, style)
 
 
 layout : Html msg -> Html msg
 layout page =
-    column [ class "container" ]
+    column [ class "container-y" ]
         [ row [ class "navbar" ]
-            [ column [ style "font-weight" "bold" ] [ text "RunApp2" ]
-            , column [ class "no-grow" ] [ text "Account" ]
+            [ column [ class "container-x" ]
+                [ row []
+                    [ compactColumn [ style "font-size" "1.5em", style "font-style" "italic" ] [ text "RunApp2" ]
+                    , column [] []
+                    , compactColumn []
+                        [ img [ style "height" "1.8em", src "assets/account.svg" ] []
+                        ]
+                    ]
+                ]
             ]
-        , page
+        , expandingRow []
+            [ column [ class "container-x" ]
+                [ page ]
+            ]
         ]
 
 
 row : List (Html.Attribute msg) -> List (Html msg) -> Html msg
 row attributes children =
     div
-        (class "row no-grow" :: attributes)
+        (class "row compact" :: attributes)
         children
 
 
 expandingRow : List (Html.Attribute msg) -> List (Html msg) -> Html msg
 expandingRow attributes children =
     div
-        (class "row" :: attributes)
+        (class "row expand" :: attributes)
         children
 
 
 column : List (Html.Attribute msg) -> List (Html msg) -> Html msg
 column attributes children =
     div
-        (class "column" :: attributes)
+        (class "column expand" :: attributes)
+        children
+
+
+compactColumn : List (Html.Attribute msg) -> List (Html msg) -> Html msg
+compactColumn attributes children =
+    div
+        (class "column compact" :: attributes)
         children
 
 
