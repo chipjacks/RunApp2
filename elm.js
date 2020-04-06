43b8e0a81f4068b13097283418c2cf9eeeb8f22d
Chip Jackson <chipjacks@gmail.com>
2020-04-06 08:07:57 -0700

    Update compiled app

--- elm.js
+++ elm.js
@@ -7726,9 +7726,10 @@ var $justinmimbs$date$Date$compare = F2(
 		var b = _v1;
 		return A2($elm$core$Basics$compare, a, b);
 	});
-var $author$project$Store$Posted = function (a) {
-	return {$: 5, a: a};
-};
+var $author$project$Store$Posted = F2(
+	function (a, b) {
+		return {$: 5, a: a, b: b};
+	});
 var $author$project$Store$State = function (activities) {
 	return {bc: activities};
 };
@@ -8673,7 +8674,7 @@ var $author$project$Store$flush = function (model) {
 		var msgs = model.b;
 		return A2(
 			$elm$core$Task$attempt,
-			$author$project$Store$Posted,
+			$author$project$Store$Posted(msgs),
 			A2(
 				$elm$core$Task$andThen,
 				function (newRemoteState) {
@@ -9255,13 +9256,146 @@ var $author$project$ActivityForm$update = F2(
 				return _Utils_Tuple2(model, $elm$core$Platform$Cmd$none);
 		}
 	});
+var $elm$core$List$takeReverse = F3(
+	function (n, list, kept) {
+		takeReverse:
+		while (true) {
+			if (n <= 0) {
+				return kept;
+			} else {
+				if (!list.b) {
+					return kept;
+				} else {
+					var x = list.a;
+					var xs = list.b;
+					var $temp$n = n - 1,
+						$temp$list = xs,
+						$temp$kept = A2($elm$core$List$cons, x, kept);
+					n = $temp$n;
+					list = $temp$list;
+					kept = $temp$kept;
+					continue takeReverse;
+				}
+			}
+		}
+	});
+var $elm$core$List$takeTailRec = F2(
+	function (n, list) {
+		return $elm$core$List$reverse(
+			A3($elm$core$List$takeReverse, n, list, _List_Nil));
+	});
+var $elm$core$List$takeFast = F3(
+	function (ctr, n, list) {
+		if (n <= 0) {
+			return _List_Nil;
+		} else {
+			var _v0 = _Utils_Tuple2(n, list);
+			_v0$1:
+			while (true) {
+				_v0$5:
+				while (true) {
+					if (!_v0.b.b) {
+						return list;
+					} else {
+						if (_v0.b.b.b) {
+							switch (_v0.a) {
+								case 1:
+									break _v0$1;
+								case 2:
+									var _v2 = _v0.b;
+									var x = _v2.a;
+									var _v3 = _v2.b;
+									var y = _v3.a;
+									return _List_fromArray(
+										[x, y]);
+								case 3:
+									if (_v0.b.b.b.b) {
+										var _v4 = _v0.b;
+										var x = _v4.a;
+										var _v5 = _v4.b;
+										var y = _v5.a;
+										var _v6 = _v5.b;
+										var z = _v6.a;
+										return _List_fromArray(
+											[x, y, z]);
+									} else {
+										break _v0$5;
+									}
+								default:
+									if (_v0.b.b.b.b && _v0.b.b.b.b.b) {
+										var _v7 = _v0.b;
+										var x = _v7.a;
+										var _v8 = _v7.b;
+										var y = _v8.a;
+										var _v9 = _v8.b;
+										var z = _v9.a;
+										var _v10 = _v9.b;
+										var w = _v10.a;
+										var tl = _v10.b;
+										return (ctr > 1000) ? A2(
+											$elm$core$List$cons,
+											x,
+											A2(
+												$elm$core$List$cons,
+												y,
+												A2(
+													$elm$core$List$cons,
+													z,
+													A2(
+														$elm$core$List$cons,
+														w,
+														A2($elm$core$List$takeTailRec, n - 4, tl))))) : A2(
+											$elm$core$List$cons,
+											x,
+											A2(
+												$elm$core$List$cons,
+												y,
+												A2(
+													$elm$core$List$cons,
+													z,
+													A2(
+														$elm$core$List$cons,
+														w,
+														A3($elm$core$List$takeFast, ctr + 1, n - 4, tl)))));
+									} else {
+										break _v0$5;
+									}
+							}
+						} else {
+							if (_v0.a === 1) {
+								break _v0$1;
+							} else {
+								break _v0$5;
+							}
+						}
+					}
+				}
+				return list;
+			}
+			var _v1 = _v0.b;
+			var x = _v1.a;
+			return _List_fromArray(
+				[x]);
+		}
+	});
+var $elm$core$List$take = F2(
+	function (n, list) {
+		return A3($elm$core$List$takeFast, 0, n, list);
+	});
 var $author$project$Store$update = F2(
 	function (msg, model) {
 		if (msg.$ === 5) {
-			var result = msg.a;
+			var sentMsgs = msg.a;
+			var result = msg.b;
 			var state = model.a;
 			var msgs = model.b;
-			return A2($author$project$Store$Model, state, _List_Nil);
+			return A2(
+				$author$project$Store$Model,
+				state,
+				A2(
+					$elm$core$List$take,
+					$elm$core$List$length(msgs) - $elm$core$List$length(sentMsgs),
+					msgs));
 		} else {
 			var state = model.a;
 			var msgs = model.b;
