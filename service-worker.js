fc5125398cf0a8ef16f6df97e6ab34e9c316087f
Chip Jackson <chip.jackson@faithlife.com>
2020-11-23 16:50:08 -0800

    Prompt users to update.

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,38 @@
+
+const VERSION = '0.1.1';
+
+const cacheName = `runapp2 v${VERSION}`;
+
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('runapp2').then(function(cache) {
-      return cache.addAll([ ]);
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/elm.js',
+        '/style.css',
+        'https://use.fontawesome.com/releases/v5.8.1/css/all.css',
+        'https://code.jquery.com/pep/0.4.3/pep.min.js',
+        '/icon.png',
+        '/manifest.json',
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheAllowlist = [cacheName];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
@@ -9,7 +40,10 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
     })
   );
 });
